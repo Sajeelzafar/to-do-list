b228ae9ad7c58b8e64ac1682e60eac729c2fe566
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-09-29 01:55:52 +0500

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,36 +1,34 @@
-import _ from 'lodash';
 import './style.css';
 
-const mainContainer = document.querySelector(".mainContainer");
+const mainContainer = document.querySelector('.mainContainer');
 
-let todoTasks = [{
-  description: "wash the dishes",
+const todoTasks = [{
+  description: 'wash the dishes',
   completed: true,
   index: 1,
 },
 {
-  description: "complete project",
+  description: 'complete project',
   completed: true,
   index: 2,
 },
 {
-  description: "play game",
+  description: 'play game',
   completed: false,
   index: 3,
 },
 {
-  description: "wash car",
+  description: 'wash car',
   completed: false,
   index: 4,
 }];
 
 document.addEventListener('DOMContentLoaded', () => {
-  todoTasks.forEach(n => {
-    let output = document.createElement('div');
-    output.classList.add("listitem");
-    output.setAttribute("id", `${n.index}`)
+  todoTasks.forEach((n) => {
+    const output = document.createElement('div');
+    output.classList.add('listitem');
+    output.setAttribute('id', `${n.index}`);
     output.innerHTML = `<div><input type="checkbox"> ${n.description}</div> <span class="listitemIcon"><i class="fa-solid fa-ellipsis-vertical"></i></span>`;
     mainContainer.appendChild(output);
-    console.log(output);
-  })
-})
+  });
+});
