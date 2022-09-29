41a00f555076a0e600347abf1a20f6654e8b4dc9
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-09-29 23:44:44 +0500

    Fix setting id in while printing new items

--- src/modules/print.js
+++ src/modules/print.js
@@ -4,7 +4,7 @@ const print = (n) => {
   const output = document.createElement('div');
   output.classList.add('listitem');
   output.setAttribute('id', `${n.index}`);
-  output.innerHTML = `<div class="listitemleft"><input type="checkbox"> <span>${n.description}</span></div> <span class="listitemIcon"><i class="fa-solid fa-ellipsis-vertical"></i></span>`;
+  output.innerHTML = `<div class="listitemleft"><input type="checkbox"> <p>${n.description}</p></div> <span class="listitemIcon"><i id="menu" class="fa-solid fa-ellipsis-vertical"></i></span>`;
   listContainer.appendChild(output);
 };
 
