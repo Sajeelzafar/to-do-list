a0623de787fe61689052248ef6e9658a32327a64
Emre Karayaman <karayaman.emre@outlook.com>
2022-10-05 17:11:48 +0300

    update test

--- src/modules/add.test.js
+++ src/modules/add.test.js
@@ -1,21 +1,31 @@
 import add from './add.js';
+import remove from './remove.js';
 
 describe('add test', () => {
-    test('adding first item', () => {
-        expect(add("Emre Book", [])).toEqual([{description: "Emre Book", completed: false, index: 1}]);
-    })
-    test('adding fourth item', () => {
-        expect(add("Sajeel Book", [{description: "Emre Book", completed: false, index: 1}, 
-        {description: "Hello", completed: false, index: 2}, 
-        {description: "Microverse", completed: false, index: 3}]))
-        .toEqual([{description: "Emre Book", completed: false, index: 1}, 
-        {description: "Hello", completed: false, index: 2}, 
-        {description: "Microverse", completed: false, index: 3},
-        {description: "Sajeel Book", completed: false, index: 4}]);
-    })
-    test('adding second item', () => {
-        expect(add("Sajeel Book", [{description: "Emre Book", completed: false, index: 1}]))
-        .toEqual([{description: "Emre Book", completed: false, index: 1},
-        {description: "Sajeel Book", completed: false, index: 2}]);
-    })
-})
+  test('adding first item', () => {
+    expect(add('Emre Book', [])).toEqual([{ description: 'Emre Book', completed: false, index: 1 }]);
+  });
+  test('adding fourth item', () => {
+    expect(add('Sajeel Book', [{ description: 'Emre Book', completed: false, index: 1 },
+      { description: 'Hello', completed: false, index: 2 },
+      { description: 'Microverse', completed: false, index: 3 }]))
+      .toEqual([{ description: 'Emre Book', completed: false, index: 1 },
+        { description: 'Hello', completed: false, index: 2 },
+        { description: 'Microverse', completed: false, index: 3 },
+        { description: 'Sajeel Book', completed: false, index: 4 }]);
+  });
+  test('adding second item', () => {
+    expect(add('Sajeel Book', [{ description: 'Emre Book', completed: false, index: 1 }]))
+      .toEqual([{ description: 'Emre Book', completed: false, index: 1 },
+        { description: 'Sajeel Book', completed: false, index: 2 }]);
+  });
+});
+
+describe('removing item', () => {
+  test('removing an item', () => {
+    add('Emre Book', []);
+    add('Sajeel Book', [{ description: 'Emre Book', completed: false, index: 1 }]);
+    expect(remove('<div id="1"></div>', [{ description: 'Emre Book', completed: false, index: 1 },
+      { description: 'Sajeel Book', completed: false, index: 2 }])).toEqual([{ description: 'Sajeel Book', completed: false, index: 1 }]);
+  });
+});
